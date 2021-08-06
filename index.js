f33e12b4d17b6846ad585fa7084b65a7e5ed69ca
Jurica Kovacevic <jurica.kovacevic@axilis.com>
2021-08-06 11:40:21 +0200

    fix: add missing parserOptions.project

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     sourceType: "module",
+    project: "./tsconfig.json",
     ecmaFeatures: {
       jsx: true,
     },
