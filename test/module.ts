bc85d19e091e9cff82d89a5af6fec0a45f83c553
Daniel Kovacs <daniel.kovacs@superbet.com>
2021-10-06 14:01:37 +0100

    test: add test case for baseUrl

---
+++ test/module.ts
@@ -0,0 +1,14 @@
+export const sqrt = (x: number): number => Math.sqrt(x);
+
+// function that takes 2 2D arrays and returns a 2D array of the sum of them
+export const merge2DArrays = (a: number[][], b: number[][]): number[][] => {
+  const result = [];
+  for (let i = 0; i < a.length; i += 1) {
+    const row = [];
+    for (let j = 0; j < a[i].length; j += 1) {
+      row.push(a[i][j] + b[i][j]);
+    }
+    result.push(row);
+  }
+  return result;
+};
