b574d71d01c0428b5684c8fc574054e6f9b13e3c
Daniel Kovacs <daniel.kovacs@superbet.com>
2021-10-06 13:48:57 +0100

    fix: move import resolve config to right place

--- index.js
+++ index.js
@@ -80,12 +80,12 @@ module.exports = {
   },
   settings: {
     "import/resolver": {
+      typescript: {},
       node: {
         extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
       },
     },
     "import/extensions": [".js", ".ts", ".mjs", ".jsx", ".tsx"],
-    typescript: {},
   },
   overrides: [
     {
